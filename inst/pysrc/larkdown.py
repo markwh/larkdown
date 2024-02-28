75f86039d37580304b52b98d4c13a5dd2432d567
Mark Hagemann <mark.hagemann@gmail.com>
2024-02-27 22:41:32 -0500

    Add larkdown.py for parsing and streaming messages

---
+++ inst/pysrc/larkdown.py
@@ -0,0 +1,62 @@
+# First go at larkdown.py
+from langserve import RemoteRunnable
+
+def parse_larkdown(text):
+    lines = text.split('\n')
+    tuples = []
+    current_speaker = None
+    current_message = []
+    ignore_block = False
+
+    for line in lines:
+        if line.startswith('<!-- ignore -->'):
+            ignore_block = True
+        elif line.startswith('<!-- endignore -->'):
+            ignore_block = False
+        elif ignore_block:
+            continue
+        elif line.startswith('>'):
+            if current_speaker:
+                tuples.append((current_speaker, '\n'.join(current_message)))
+            current_speaker = line[1:].strip()
+            current_message = []
+        else:
+            current_message.append(line)
+
+    # Don't forget to add the last speaker and message
+    if current_speaker and current_message:
+        tuples.append((current_speaker, '\n'.join(current_message)))
+
+    return tuples
+
+
+def stream_to_file(messages, endpoint, file):
+    with open(file, 'w') as f:
+        # Append prompt for new AI user message
+        f.write('\n\n>AI\n')
+        for chunk in endpoint.stream(messages):
+            f.write(chunk)
+        # Append prompt for new Human user message
+        f.write('\n\n>Human\n')
+
+# main function will need to use argparse to parse the command line arguments
+def main():
+    import argparse
+
+    # arguments are the file and the endpoint url
+    parser = argparse.ArgumentParser(description='Convert larkdown to a stream of messages')
+    parser.add_argument('file', help='The file to convert')
+    parser.add_argument('endpoint_url', help='The langserve endpoint to use')
+
+    args = parser.parse_args()
+
+    with open(args.file, 'r') as f:
+        text = f.read()
+    
+    messages = parse_larkdown(text)
+    endpoint = RemoteRunnable(args.endpoint_url)
+    stream_to_file(messages, endpoint, args.file)
+
+
+if __name__ == '__main__':
+    main()
