f1dfb21e2b9947c1851ea9ed721643c6ab1b5d99
Mark Hagemann <mark.hagemann@gmail.com>
2024-03-06 21:52:46 -0500

    Add functionality for knitting documents to messages

--- R/stream.R
+++ R/stream.R
@@ -7,8 +7,7 @@ ld_stream_current <- function() { # Possible naming convention?
   ld <- import_larkdown()
   langserve <- import("langserve")
 
-  doccontext <- rstudioapi::getActiveDocumentContext()
-  stream_path <- normalizePath(doccontext$path)
+  stream_path <- current_document()
   doc_text <- paste(readLines(stream_path), collapse = "\n")
   ld_messages <- ld$parse_larkdown(doc_text)
 
@@ -16,3 +15,37 @@ ld_stream_current <- function() { # Possible naming convention?
   rstudioapi::documentSave()
   ld$stream_to_file(messages = ld_messages, endpoint = endpoint, file = stream_path)
 }
+
+ld_knit_and_stream_current <- function() {
+  ld <- import_larkdown()
+  langserve <- import("langserve")
+  
+  infile <- current_document()
+  
+  ld_messages <- knit_to_messages(infile)
+  endpoint <- langserve$RemoteRunnable(getOption("ld_endpoint_url"))
+  ld$stream_to_file(messages = ld_messages, endpoint = endpoint, file = infile)
+}
+
+knit_to_messages <- function(file = current_document()) {
+  
+  ld <- import_larkdown()
+  
+  # render current document to tempfile md
+  outfile <- tempfile(fileext = ".md")
+  knitr::knit(file, outfile)
+  
+  # parse messages
+  doc_text <- paste(readLines(outfile), collapse = "\n")
+  ld_messages <- ld$parse_larkdown(doc_text)
+  
+  ld_messages
+}
+
+current_document <- function() {
+  rstudioapi::documentSave()
+  doccontext <- rstudioapi::getActiveDocumentContext()
+  out <- normalizePath(doccontext$path)
+  out
+}
+
