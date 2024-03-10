4a05a9c5db2038c6b23f07b95beea1ca2c6d19a4
Mark Hagemann <mark.hagemann@gmail.com>
2024-03-09 23:22:40 -0500

    rename functions to stream_current, knit_and_stream_current; added utility functions for larkdown file creation

--- R/stream.R
+++ R/stream.R
@@ -3,7 +3,7 @@
 #' Parse the current document and stream response back to it
 #' 
 #' @importFrom reticulate import
-ld_stream_current <- function() { # Possible naming convention?
+stream_current <- function(endpoint_url=getOption("ld_endpoint_url")) { # Possible naming convention?
   ld <- import_larkdown()
   langserve <- import("langserve")
 
@@ -11,12 +11,12 @@ ld_stream_current <- function() { # Possible naming convention?
   doc_text <- paste(readLines(stream_path), collapse = "\n")
   ld_messages <- ld$parse_larkdown(doc_text)
 
-  endpoint <- langserve$RemoteRunnable(getOption("ld_endpoint_url"))
+  endpoint <- langserve$RemoteRunnable(endpoint_url)
   rstudioapi::documentSave()
   ld$stream_to_file(messages = ld_messages, endpoint = endpoint, file = stream_path)
 }
 
-ld_knit_and_stream_current <- function() {
+knit_and_stream_current <- function() {
   ld <- import_larkdown()
   langserve <- import("langserve")
   
@@ -42,10 +42,4 @@ knit_to_messages <- function(file = current_document()) {
   ld_messages
 }
 
-current_document <- function() {
-  rstudioapi::documentSave()
-  doccontext <- rstudioapi::getActiveDocumentContext()
-  out <- normalizePath(doccontext$path)
-  out
-}
 
--- R/utils.R
+++ R/utils.R
@@ -1,28 +1,33 @@
-#' Utility function to generate a random number
-#'
-#' @return A random number between 0 and 1
-generate_random_number <- function() {
-  return(runif(1))
-}
+# R/utils.R
+# Utilty functions
 
-#' Utility function to format a date
-#'
-#' @param date The date to format
-#' @return The formatted date
-format_date <- function(date) {
-  return(format(date, "%Y-%m-%d"))
-}
-
-# TODO: make an Rmarkdown template for notebook files
+#' Launches a journaling session for the current day. 
+#' 
 #' @export
-boot_up <- function(base_path = Sys.getenv("NOTEBOOK_DIR")) {
+journal <- function(base_path = Sys.getenv("NOTEBOOK_DIR")) {
 
   filename <- file.path(base_path,
                         sprintf("notebook%s.Rmd", format(Sys.Date(), "%Y%m%d")))
-  if (!file.exists(filename)) file.create(filename)
+  if (file.exists(filename)) {
+    rstudioapi::documentOpen(filename)
+  } else {
+    new_larkdown(filename, template = "larkdown-journal", package = "larkdown")
+  }
+  return(TRUE)
+}
+
+#' Creates a new Rmarkdown file with a Larkdown template, then opens the file
+#'
+#' @export
+new_larkdown <- function(filename = ts_filename(prefix = "larkdown"), 
+                         template="larkdown-file", package = "larkdown") {
+  
+  rmarkdown::draft(filename, template = template, package = package, edit = FALSE)
   rstudioapi::documentOpen(filename)
 }
 
+#' Wrapper for `reticulate::import_from_path()` to pull in larkdown module
+#' 
 #' @export
 import_larkdown <- function() {
   reticulate::import_from_path(
@@ -31,7 +36,28 @@ import_larkdown <- function() {
     delay_load = TRUE)
 }
 
+#' Sets the `ld_endpoint_url` option
+#'
 #' @export
 register_endpoint <- function(endpoint_url) {
   options(ld_endpoint_url = endpoint_url)
+}
+
+#' Timestamp filename creator
+#'
+#' @export
+ts_filename <- function(prefix = "", suffix = "", ext = ".Rmd") {
+  basename <- format(Sys.time(), "%Y%m%d_%H%M%S")
+  out <- paste0(prefix, basename, suffix, ext)
+  out
+}
+
+#' Returns the full path of the current document, saving it first.
+#' 
+#' @export
+current_document <- function() {
+  rstudioapi::documentSave()
+  doccontext <- rstudioapi::getActiveDocumentContext()
+  out <- normalizePath(doccontext$path)
+  out
 }
